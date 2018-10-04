f3acbeec7469d92719bccbb7e6db10f58e33be46
Dimitrios Lytras <dnlytras@gmail.com>
2018-10-04 18:09:08 +0300

    docs: Fix path names containing trailing slashes
    Change-type: minor
    Signed-off-by: Dimitrios Lytras <dimitrios@resin.io>

--- theme/landr-theme-fin/pages/Doc.js
+++ theme/landr-theme-fin/pages/Doc.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import _ from 'lodash';
 import { getSiteProps } from '@resin.io/react-static';
 
 import DocsWrapper from '../pages/Docs';
 import Doc from '../components/Doc';
 
 export default getSiteProps(({ docs, location }) => {
-	const key = location.pathname.replace('/docs/', '');
+	let key = location.pathname.replace('/docs/', '');
+	key = _.trimEnd(key, '/');
+
 	const document = <Doc {...docs.find(({ slug }) => slug === key)} />;
 	return <DocsWrapper>{document}</DocsWrapper>;
 });
